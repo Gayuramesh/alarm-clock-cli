d0cc6b9ccee60d4f2458da2ad598c83a381f96b1
Gayuramesh <89973403+Gayuramesh@users.noreply.github.com>
2026-07-22 12:26:53 +0530

    Upgrade alarm model

--- alarm-clock-cli/alarm_clock/alarm.py
+++ alarm-clock-cli/alarm_clock/alarm.py
@@ -1,11 +1,19 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
 class Alarm:
-    """
-        Represents a single alarm.
-    """
 
-    time: str
+    alarm_time: datetime
+
     triggered: bool = False
+
+    @property
+    def display_time(self):
+
+        return self.alarm_time.strftime("%H:%M")
+
+    def __str__(self):
+
+        return self.display_time
